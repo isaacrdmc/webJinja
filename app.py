be0ec6aad2d760974852df297eec60f6db70305d
Isaac <55609604+isaacrdmc@users.noreply.github.com>
2025-03-02 13:45:26 -0600

    He agregado comentarios para explicar cada parte del sitio

--- app.py
+++ app.py
@@ -6,67 +6,91 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length
 from werkzeug.security import generate_password_hash, check_password_hash
 
-
+# ? Configuraión de la BD y flask
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secretkey'  # Clave secreta
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SECRET_KEY'] = 'secretkey'  # Creamos la clave secreta
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'  # Definimos el archivo de la BD que se llamara 'db.sqlite'
 db = SQLAlchemy(app)
 
-# Modelo de usuario
+
+# ? Modelo de usuario
+# Le decimos que se valla a la tabla del usuario que sera donde almacenara lo siguiente:
 class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     usuario = db.Column(db.String(15), unique=True, nullable=False)
     contrasena = db.Column(db.String(128), nullable=False)
 
-# Creación de la BD
+# ? Creamos la tabla 'Usuario' si es que aun no existe
 with app.app_context():
     db.create_all()
 
-# Formulario
+
+
+# ? Creamos la sección del Formulario
 class MiFormularioLogin(FlaskForm):
-    usuario = StringField(
+    usuario = StringField(      # Campo de entrada
         'Ingresa el usuario', 
-        validators=[InputRequired('Usuario requerido'), Length(min=5, max=25)]
+        validators=[InputRequired('Usuario requerido'), Length(min=5, max=25)]      # Hacemos al comapo obligatorio para que no se lo salte
     )
-    contraseña = PasswordField(
+    contraseña = PasswordField(      # Campo de entrada
         'Ingresa la contraseña', 
-        validators=[InputRequired('Contraseña requerida')]
+        validators=[InputRequired('Contraseña requerida')]      # Hacemos al comapo obligatorio para que no se lo salte
     )
+
+    # * Botones de funcion dentro del ormulario
     enviarmos = SubmitField('Iniciar sesión')
     registro = SubmitField('Registro')
 
-# Ruta para el formulario de login
+
+
+
+# ? Deinimos la ruta para el formulario de login
 @app.route('/form', methods=['GET', 'POST'])
 def form():
+    # Mandamos a llamar la clase del formulario
     form = MiFormularioLogin()
+
+    # Si se ha enviado se analizara todo lo siguiente
     if form.validate_on_submit():
+        # Filtramos para buscar al usuarios dentro de la BD
         usuario = Usuario.query.filter_by(usuario=form.usuario.data).first()
+
+        # Ahora vefiricamos que la contraseña exista y si existe le damos la bienvenida
         if usuario and check_password_hash(usuario.contrasena, form.contraseña.data):
             return redirect(url_for('bienvenido', usuario=form.usuario.data))
+        
+            # En caso contrario le informamos de eso
         else:
             flash('Usuario o contraseña incorrectos.', 'error')
             return redirect(url_for('form'))
 
     return render_template('login.html', form=form)
 
-# Ruta para la pantalla de bienvenida
+
+
+# ? Ruta para la pantalla de bienvenida
 @app.route('/bienvenido/<usuario>')
 def bienvenido(usuario):
     return render_template('bienvenido.html', usuario=usuario)
 
-# Ruta de registro
+
+
+# ? Ruta para el registro del usuario
 @app.route('/registro/', methods=['GET', 'POST'])
 def registro():
     form = MiFormularioLogin()
+
+    # Cuando el formualrio se envie primero validamos si ya existe este usuario en la BD
     if form.validate_on_submit():
         usuario_existente = Usuario.query.filter_by(usuario=form.usuario.data).first()
         if usuario_existente:
             flash(f'El usuario {form.usuario.data} ya está registrado.', 'error')
             return redirect(url_for('registro'))
 
+        # Si no existe lo alamcenamos dentro de la BD
         nuevo_usuario = Usuario(
             usuario=form.usuario.data, 
-            contrasena=generate_password_hash(form.contraseña.data)
+            contrasena=generate_password_hash(form.contraseña.data) # * Ciframos la contraseña con un hash por seguridad
         )
         db.session.add(nuevo_usuario)
         db.session.commit()
@@ -75,5 +99,7 @@ def registro():
 
     return render_template('registro.html', form=form)
 
+
+# ? Ejecutamos el sitio
 if __name__ == '__main__': 
     app.run(debug=True)
